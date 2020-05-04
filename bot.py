ed0002ab846d854202776371bb6cfbc2d4524a50
Bogdan_Detrov <vernitynout@gmail.com>
2020-05-04 17:51:31 +1000

    Добавил присвоение юзеру рандомной emoji из списка USER_EMOJI

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from glob import glob
 import logging
 from random import choice
 
+from emoji import emojize
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
@@ -10,22 +11,32 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log')
 
-def greet_user(bot, update):
-    text = 'Вызван /start'
-    logging.info(text)
+def greet_user(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_data['emo'] = emo
+    text = 'Привет {}'.format(emo)
     update.message.reply_text(text)
 
-def talk_to_me(bot, update):
-    user_text = "Привет {}! Ты написал: {}".format(update.message.chat.first_name, 
-                 update.message.text)
+def talk_to_me(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_text = "Привет {} {}! Ты написал: {}".format(update.message.chat.first_name, 
+                user_data['emo'], update.message.text)
     logging.info("User: %s, Chat id: %s, Message: %s",update.message.chat.username,
                  update.message.chat.id, update.message.text)
     update.message.reply_text(user_text)
 
-def send_hedgehog_picture(bot, update):
+def send_hedgehog_picture(bot, update, user_data):
     hedgehog_list = glob('images/hedgehog*.jp*g')
     hedgehog_pic = choice(hedgehog_list)
     bot.send_photo(chat_id=update.message.chat_id, photo=open(hedgehog_pic, 'rb'))
+
+def get_user_emo(user_data):
+    if 'emo' in user_data:
+        return user_data['emo']
+    else:
+       user_data['emo'] = emojize(choice(settings.USER_EMOJI), use_aliases=True)
+       return user_data['emo']
+
 
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
@@ -33,10 +44,10 @@ def main():
     logging.info('Bot starts')
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('start', greet_user))
-    dp.add_handler(CommandHandler("hedgehog", send_hedgehog_picture))
+    dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
+    dp.add_handler(CommandHandler("hedgehog", send_hedgehog_picture, pass_user_data=True))
 
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
     
 
     mybot.start_polling()
