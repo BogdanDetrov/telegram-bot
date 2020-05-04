2ea2f6151f70ddb426822365cbc7531f94aba392
Bogdan_Detrov <vernitynout@gmail.com>
2020-05-04 17:12:53 +1000

    Добавил команду /hedgehog, при получении данной команды от пользователя, бот отправляет пользователю рандомную картинку ежика из папки images

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from glob import glob
 import logging
+from random import choice
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 import settings
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
@@ -18,6 +22,11 @@ def talk_to_me(bot, update):
                  update.message.chat.id, update.message.text)
     update.message.reply_text(user_text)
 
+def send_hedgehog_picture(bot, update):
+    hedgehog_list = glob('images/hedgehog*.jp*g')
+    hedgehog_pic = choice(hedgehog_list)
+    bot.send_photo(chat_id=update.message.chat_id, photo=open(hedgehog_pic, 'rb'))
+
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
     
@@ -25,6 +34,8 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler("hedgehog", send_hedgehog_picture))
+
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     
 
