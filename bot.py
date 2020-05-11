a2dbb0b1b8ff384727a572e37702279c1a6ee823
Bogdan_Detrov <vernitynout@gmail.com>
2020-05-12 02:23:25 +1000

    Добавил анкету для бота

--- bot.py
+++ bot.py
@@ -16,7 +16,22 @@ def main():
     logging.info('Bot starts')
 
     dp = mybot.dispatcher
+    anketa = ConversationHandler(
+    entry_points = [RegexHandler('^(Заполнить анкету)$', anketa_start, 
+                    pass_user_data=True)],
+    states = {
+        'name': [MessageHandler(Filters.text, anketa_get_name, pass_user_data=True)],
+        'rating': [RegexHandler('^(1|2|3|4|5)$', anketa_rating, pass_user_data=True)],
+        'comment': [MessageHandler(Filters.text, anketa_comment, pass_user_data=True),
+                    CommandHandler('skip', anketa_skip_comment, pass_user_data=True)]
+        },
+    fallbacks = [MessageHandler(Filters.text | Filters.video | Filters.photo | 
+                                Filters.document, dontknow, pass_user_data=True
+        )]
+    )
+    
     dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
+    dp.add_handler(anketa)
     dp.add_handler(CommandHandler("cat", send_cat_picture, pass_user_data=True))
     dp.add_handler(RegexHandler('^(Прислать котика)$', send_cat_picture, pass_user_data=True))
     dp.add_handler(RegexHandler('^(Сменить аватарку)$', change_avatar, pass_user_data=True))
@@ -24,22 +39,8 @@ def main():
     dp.add_handler(MessageHandler(Filters.location, get_location, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.photo, check_user_photo, pass_user_data=True))
 
-    anketa = ConversationHandler(
-        entry_points = [RegexHandler('^(Заполнить анкету)$', anketa_start, 
-                       pass_user_data=True)],
-        states = {
-            'name': [MessageHandler(Filters.text, anketa_get_name, pass_user_data=True)],
-            'rating': [RegexHandler('^(1|2|3|4|5)$', anketa_rating, pass_user_data=True)],
-            'comment': [MessageHandler(Filters.text, anketa_comment, pass_user_data=True),
-                        CommandHandler('skip', anketa_skip_comment, pass_user_data=True)]
-        },
-        fallbacks = []
-    )
-
-    dp.add_handler(anketa)
     dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
     
-
     mybot.start_polling()
     mybot.idle()
 
